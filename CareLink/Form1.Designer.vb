1761d52d75ee9a8de8562ad4729c797d9ab139e7
Paul M Cohen <paul1956@users.noreply.github.com>
2021-04-06 18:28:37 -0700

    Initial commit

---
+++ CareLink/Form1.Designer.vb
@@ -0,0 +1,66 @@
+﻿Imports Microsoft.Web.WebView2.WinForms
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.AddressBar = New System.Windows.Forms.TextBox()
+        Me.WebView21 = New WebView2()
+        Me.SuspendLayout()
+        '
+        'AddressBar
+        '
+        Me.AddressBar.Dock = System.Windows.Forms.DockStyle.Top
+        Me.AddressBar.Location = New System.Drawing.Point(0, 0)
+        Me.AddressBar.Name = "AddressBar"
+        Me.AddressBar.Size = New System.Drawing.Size(933, 23)
+        Me.AddressBar.TabIndex = 1
+        '
+        'WebView21
+        '
+        Me.WebView21.CreationProperties = Nothing
+        Me.WebView21.Location = New System.Drawing.Point(0, 23)
+        Me.WebView21.Name = "WebView21"
+        Me.WebView21.Size = New System.Drawing.Size(800, 450)
+        Me.WebView21.Source = New Uri("https://carelink.minimed.com/app/login")
+        Me.WebView21.TabIndex = 0
+        Me.WebView21.ZoomFactor = 1.0R
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(933, 519)
+        Me.Controls.Add(Me.AddressBar)
+        Me.Controls.Add(Me.WebView21)
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
+    Friend WithEvents AddressBar As TextBox
+End Class
---
+++ CareLink/Form1.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Text.RegularExpressions
+
+Imports Microsoft.Web.WebView2.Core
+
+Imports MSHTML
+
+Public Class Form1
+    WithEvents Timer1 As New Timer
+
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Await WebView21.EnsureCoreWebView2Async()
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        WebView21.Size = Me.ClientSize - New Size(WebView21.Location)
+    End Sub
+
+    Private Async Function parseMyHtml() As Task(Of String)
+        Dim htmlToParse = Await WebView21.ExecuteScriptAsync("document.documentElement.outerHTML;")
+        htmlToParse = Regex.Unescape(htmlToParse)
+        htmlToParse = htmlToParse.Remove(0, 1)
+        htmlToParse = htmlToParse.Remove(htmlToParse.Length - 1, 1)
+
+        Dim i As Integer = htmlToParse.IndexOf("<div class=""sensor-value""", StringComparison.InvariantCulture)
+        If i >= 0 Then
+            i = htmlToParse.IndexOf(">", i + 1, StringComparison.InvariantCulture)
+            i += 1
+            Dim lessThanIndex As Integer = htmlToParse.IndexOf("<", i, StringComparison.InvariantCulture)
+            Return htmlToParse.Substring(i, lessThanIndex - i)
+        End If
+        Return ""
+    End Function
+
+    Private Async Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        Await parseMyHtml()
+    End Sub
+
+    Private Sub WebView21_ContentLoading(sender As Object, e As CoreWebView2ContentLoadingEventArgs) Handles WebView21.ContentLoading
+        Stop
+    End Sub
+
+    Private Sub WebView21_NavigationCompleted(sender As Object, e As CoreWebView2NavigationCompletedEventArgs) Handles WebView21.NavigationCompleted
+
+    End Sub
+
+    Private Sub WebView21_NavigationStarting(sender As Object, e As CoreWebView2NavigationStartingEventArgs) Handles WebView21.NavigationStarting
+        AddressBar.Text = e.Uri
+    End Sub
+
+    Private Sub WebView21_SourceChanged(sender As Object, e As CoreWebView2SourceChangedEventArgs) Handles WebView21.SourceChanged
+
+        If Not Timer1.Enabled Then
+            Timer1.Stop() 'Timer starts functioning
+            Timer1.Interval = 5000
+        End If
+        Timer1.Start() 'Timer starts functioning
+
+    End Sub
+
+    Private Sub WebView21_WebMessageReceived(sender As Object, e As CoreWebView2WebMessageReceivedEventArgs) Handles WebView21.WebMessageReceived
+        Stop
+    End Sub
+
+End Class
