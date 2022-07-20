ba0a0a08b4521b7d36da47da90e82e0115cb377c
Paul M Cohen <paul1956@users.noreply.github.com>
2022-07-20 13:42:06 -0700

    Add additional message

--- src/CareLink/Utilities/UserMessageHandler.vb
+++ src/CareLink/Utilities/UserMessageHandler.vb
@@ -15,9 +15,9 @@ Friend Module UserMessageHandler
     Friend ReadOnly _messages As New Dictionary(Of String, String) From {
                             {"BC_MESSAGE_CONFIRM_SENSOR_SIGNAL_CHECK_BG", "No calibration occured.Confirm sensor signal. Check BG again to calibrate sensor."},
                             {"BC_MESSAGE_DELIVERY_STOPPED_SG_APPROACHILG_LOW_LIMIT_CHECK_BG", "Suspend before low. Delivery stopped. Sensor glucose approaching Low Limit. Check BG."},
+                            {"BC_SID_BASAL_STARTED_SMART_GUARD", "Basal started SmartGuard."},
                             {"BC_SID_BG_REQUIRED_CONTENT", "BG required. Enter a new BG for Auto Mode."},
                             {"BC_SID_BOLUS_ENTRY_TIMED_OUT", "Bolus not delivered. Bolus entry timed out before delivery. If bolus was intended, enter values again."},
-                            {"BC_SID_BASAL_STARTED_SMART_GUARD", "Basal started SmartGuard."},
                             {"BC_SID_CHECK_BG_AND_CALIBRATE_SENSOR", "Calibrate now. Check BG and calibrate sensor."},
                             {"BC_SID_CHECK_BG_CONSIDER_TESTING_KETONES_CHANGE_RESERVOIR", "Check BG and consider testing ketones and changing reservoir."},
                             {"BC_SID_DELIVERY_STOPPED_INSERT_NEW_BATTERY", "Insert battery. Delivery stopped. Insert a new battery now."},
@@ -66,6 +66,7 @@ Friend Module UserMessageHandler
                             {"BC_MESSAGE_TIME_REMAINING_CHANGE_RESERVOIR", "Low Reservoir (0) units remaining. Change reservoir.:unitsRemaining"},
                             {"BC_SID_BASAL_DELIVERY_RESUMED_AT_X_AFTER_LOW_SUSPEND", "Basal delivery resumed at (0) after suspend by sensor, Check BG.:secondaryTime"},
                             {"BC_SID_CHECK_BG_AND_CALIBRATE_SENSOR_TO_RECEIVE", "Calibrate by (0). Check BG and calibrate to continuing receiving sensor information.:secondaryTime"},
+                            {"BC_SID_HIGH_SG_CHECK_BG", "$Alert on high (0) (units). High sensor glucose. Check BG.:sg"},
                             {"BC_SID_LOW_SD_CHECK_BG", $"Alert on low (0) (units). Low sensor glucose. Check BG.:sg"},
                             {"BC_SID_LOW_SG_INSULIN_DELIVERY_SUSPENDED_SINCE_X_CHECK_BG", "Alert on low (0) (units). Insulin delivery suspended since (secondaryTime). Check BG.:sg"},
                             {"BC_SID_THREE_DAYS_SINCE_LAST_SET_CHANGE", "(0) days since last set change:lastSetChange"}
