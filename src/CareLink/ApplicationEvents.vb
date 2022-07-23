953dc50b7a76de8b1178ec949df765f426e8186a
Paul M Cohen <paul1956@users.noreply.github.com>
2022-07-23 01:53:38 -0700

    Update Application Events

--- src/CareLink/ApplicationEvents.vb
+++ src/CareLink/ApplicationEvents.vb
@@ -1,7 +1,7 @@
-﻿Imports System.IO
-Imports System.Net
-Imports System.Text
-Imports Microsoft.VisualBasic.ApplicationServices
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
 Imports Microsoft.VisualBasic.Devices
 
 Namespace My
@@ -20,7 +20,5 @@ Namespace My
 
         End Sub
 
-        'Private Sub MyApplication_UnhandledException(sender As Object, e As UnhandledExceptionEventArgs) Handles Me.UnhandledException
-        'End Sub
     End Class
 End Namespace
