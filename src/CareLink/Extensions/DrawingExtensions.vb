d97a0473d11286b66dd98283aec1c2d7ad80b719
Paul M Cohen <paul1956@users.noreply.github.com>
2022-10-28 00:29:45 -0700

    Fix time to calibrate graph

--- src/CareLink/Extensions/DrawingExtensions.vb
+++ src/CareLink/Extensions/DrawingExtensions.vb
@@ -6,10 +6,10 @@ Imports System.Runtime.CompilerServices
 
 Friend Module DrawingExtensions
 
-    Private Function GetColorFromTimeToNextCalib(timeToNextCalibHours As Double) As Color
-        If timeToNextCalibHours <= 1.9 Then
+    Private Function GetColorFromTimeToNextCalib(hoursToNextCalibration As Double) As Color
+        If hoursToNextCalibration <= 1.9 Then
             Return Color.Red
-        ElseIf timeToNextCalibHours < 4 Then
+        ElseIf hoursToNextCalibration < 4 Then
             Return Color.Yellow
         Else
             Return Color.Lime
@@ -17,14 +17,17 @@ Friend Module DrawingExtensions
     End Function
 
     <Extension>
-    Friend Function DrawCenteredArc(backImage As Bitmap, TimeToNextCalibration As Double, arcPercentage As Double, Optional colorTable As IReadOnlyDictionary(Of String, Color) = Nothing, Optional segmentName As String = "") As Bitmap
+    Friend Function DrawCenteredArc(backImage As Bitmap, minutesToNextCalibration As Double, Optional colorTable As IReadOnlyDictionary(Of String, Color) = Nothing, Optional segmentName As String = "") As Bitmap
+
+        Dim arcPercentage As Double = minutesToNextCalibration / 720
+
         If arcPercentage < Double.Epsilon Then
             Return backImage
         End If
         Dim targetImage As Bitmap = backImage
         Dim myGraphics As Graphics = Graphics.FromImage(targetImage)
         myGraphics.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
-        Dim newPen As Pen = If(colorTable Is Nothing, New Pen(GetColorFromTimeToNextCalib(TimeToNextCalibration), 2), New Pen(colorTable(segmentName), 5))
+        Dim newPen As Pen = If(colorTable Is Nothing, New Pen(GetColorFromTimeToNextCalib(minutesToNextCalibration / 60), 2), New Pen(colorTable(segmentName), 5))
         Dim rect As New Rectangle(1, 1, backImage.Width - 2, backImage.Height - 2)
         myGraphics.DrawArc(newPen, rect, -90, -CInt(360 * arcPercentage))
         Return targetImage
