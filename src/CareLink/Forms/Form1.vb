51b2ee03c3c674473d2095a27df8cc4671aad2a8
Paul M Cohen <paul1956@users.noreply.github.com>
2023-06-01 14:49:47 -0700

    merge

--- src/CareLink/Forms/Form1.vb
+++ src/CareLink/Forms/Form1.vb
@@ -2458,7 +2458,7 @@ Public Class Form1
             Dim sg As Single = s_lastSgRecord.sg
             Dim str As String = s_lastSgRecord.sg.ToString
             Dim fontToUse As New Font("Trebuchet MS", 10, FontStyle.Regular, GraphicsUnit.Pixel)
-            Dim color As Color = color.White
+            Dim color As Color = Color.White
             Dim bgColor As Color
             Dim notStr As New StringBuilder
 
@@ -2466,16 +2466,16 @@ Public Class Form1
                 Using g As Graphics = Graphics.FromImage(bitmapText)
                     Select Case sg
                         Case <= TirLowLimit(ScalingNeeded)
-                            bgColor = color.Yellow
+                            bgColor = Color.Yellow
                             If _showBalloonTip Then
                                 Me.NotifyIcon1.ShowBalloonTip(10000, $"{ProjectName}{TmChar} Alert", $"SG below {TirLowLimit(ScalingNeeded)} {BgUnitsString}", Me.ToolTip1.ToolTipIcon)
                             End If
                             _showBalloonTip = False
                         Case <= TirHighLimit(ScalingNeeded)
-                            bgColor = color.Green
+                            bgColor = Color.Green
                             _showBalloonTip = True
                         Case Else
-                            bgColor = color.Red
+                            bgColor = Color.Red
                             If _showBalloonTip Then
                                 Me.NotifyIcon1.ShowBalloonTip(10000, $"{ProjectName}{TmChar} Alert", $"SG above {TirHighLimit(ScalingNeeded)} {BgUnitsString}", Me.ToolTip1.ToolTipIcon)
                             End If
