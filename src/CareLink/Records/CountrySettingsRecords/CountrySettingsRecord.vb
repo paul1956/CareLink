8e87d456e762c81bfc59baf6c1e7b1a0bb466f9b
Paul M Cohen <paul1956@users.noreply.github.com>
2023-06-08 19:29:23 -0700

    Cleanup unused imports

--- src/CareLink/Records/CountrySettingsRecords/CountrySettingsRecord.vb
+++ src/CareLink/Records/CountrySettingsRecords/CountrySettingsRecord.vb
@@ -2,8 +2,6 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
-Imports DocumentFormat.OpenXml.Wordprocessing
-
 Public Class CountrySettingsRecord
 
     Public Sub New()
