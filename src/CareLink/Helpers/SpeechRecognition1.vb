2c69b2e77929ba594606a98e0d6c2066ee779aec
Paul M Cohen <paul1956@users.noreply.github.com>
2023-07-27 14:59:15 -0700

    More improvements in speech messages

--- src/CareLink/Helpers/SpeechRecognition1.vb
+++ src/CareLink/Helpers/SpeechRecognition1.vb
@@ -42,23 +42,9 @@ Friend Module SpeechSupport
         End If
     End Sub
 
-    Private Function GetTrendText() As String
-        Dim arrows As String = Form1.LabelTrendArrows.Text
-        Dim arrowCount As Integer
-        Select Case True
-            Case arrows.Contains("↓"c)
-                arrowCount = arrows.Count("↓"c)
-                Return $" and is trending down with {arrowCount} Arrow{If(arrowCount > 1, "s", "")}"
-            Case arrows.Contains("↑"c)
-                arrowCount = arrows.Count("↑"c)
-                Return $" and is trending up with {arrowCount} Arrow{If(arrowCount > 1, "s", "")}"
-            Case Else
-                Return " with no trend arrows"
-        End Select
-    End Function
-
-    Private Sub sre_AudioSignalProblemOccurred(sender As Object, e As AudioSignalProblemOccurredEventArgs)
+    Private Sub AudioSignalProblemOccurred(sender As Object, e As AudioSignalProblemOccurredEventArgs)
         If s_shuttingDown OrElse s_speechErrorReported Or s_sre Is Nothing Then Exit Sub
+        Dim errorMsg As String = "Listening"
         Select Case e.AudioSignalProblem
             Case AudioSignalProblem.NoSignal
                 If Not s_speechErrorReported Then
@@ -74,142 +60,41 @@ Friend Module SpeechSupport
                     MsgBox("Audio signal problem", details.ToString, MsgBoxStyle.OkOnly Or MsgBoxStyle.Information, "Audio Error", 15, page)
                     s_speechErrorReported = page.Verification.Checked
                 End If
-                Form1.StatusStripSpeech.Text = $"Speech signal issue {e.AudioSignalProblem}"
+                errorMsg = $"Speech: signal issue {e.AudioSignalProblem}"
             Case AudioSignalProblem.TooNoisy
-                Form1.StatusStripSpeech.Text = "There is too much noise to understand you"
+                errorMsg = "Speech: Too much noise to understand you"
             Case AudioSignalProblem.TooLoud
-                Form1.StatusStripSpeech.Text = "You are speaking too loud"
+                errorMsg = "Speech: You are speaking too loud"
             Case AudioSignalProblem.TooFast
-                Form1.StatusStripSpeech.Text = "Please speak slower"
+                errorMsg = "Speech: Please speak slower"
             Case AudioSignalProblem.TooSlow
-                Form1.StatusStripSpeech.Text = "Please speak faster"
-            Case AudioSignalProblem.None, AudioSignalProblem.TooSoft
-                Form1.StatusStripSpeech.Text = "Listening"
+                errorMsg = "Speech: Please speak faster"
+            Case AudioSignalProblem.TooSoft
+                errorMsg = "Speech: Please speak louder"
+            Case Else
         End Select
-
-    End Sub
-
-    Friend Sub CancelSpeechRecognition()
-        If s_sre IsNot Nothing Then
-            RemoveHandler s_sre.AudioSignalProblemOccurred, AddressOf sre_AudioSignalProblemOccurred
-            RemoveHandler s_sre.SpeechRecognized, AddressOf sre_SpeechRecognized
-            s_sre.RecognizeAsyncCancel()
-            s_sre.Dispose()
-            s_sre = Nothing
-            s_speechUserName = ""
-            Form1.StatusStripSpeech.Text = ""
-        End If
-    End Sub
-
-    Friend Sub InitializeAudioAlerts()
-        If s_ss Is Nothing Then
-            s_ss = New SpeechSynthesizer()
-            s_ss.SetOutputToDefaultAudioDevice()
+        If Not Form1.StatusStripSpeech.Text.StartsWith("Speech:") Then
+            Form1.StatusStripSpeech.Text = errorMsg
         End If
 
     End Sub
 
-    Friend Sub InitializeSpeechRecognition()
-        Dim oldUserName As String = s_speechUserName
-        If s_speechUserName = s_firstName AndAlso s_sre IsNot Nothing Then
-            Exit Sub
-        End If
-        CancelSpeechRecognition()
-
-        Try
-            s_speechWakeWordFound = False
-
-            Dim culture As New CultureInfo("en-us")
-            s_sre = New SpeechRecognitionEngine(culture)
-            s_sre.SetInputToDefaultAudioDevice()
-
-            Dim gb_Attention As New GrammarBuilder With {.Culture = culture}
-            gb_Attention.Append(s_careLinkLower)
-            s_sre.LoadGrammarAsync(New Grammar(gb_Attention))
-
-            'Dim gb_StartStop As New GrammarBuilder With {.Culture = culture}
-            'gb_StartStop.Append("alerts")
-            'gb_StartStop.Append(New Choices("off", "on"))
-            's_sre.LoadGrammarAsync(New Grammar(gb_StartStop))
-
-            Dim gb_what As New GrammarBuilder With {.Culture = culture}
-            gb_what.Append("What")
-            gb_what.Append(New Choices("can I say", "is my SG", "is my BG", "is my Blood Sugar", "is my Blood Glucose"))
-            s_sre.LoadGrammarAsync(New Grammar(gb_what))
-
-            Dim gb_tellMe As New GrammarBuilder With {.Culture = culture}
-            gb_tellMe.Append("Tell me")
-            gb_tellMe.Append($"{s_firstName}'s")
-            gb_tellMe.Append(New Choices("SG", "BG", "Blood Sugar", "Blood Glucose"))
-            s_sre.LoadGrammarAsync(New Grammar(gb_tellMe))
-
-            'Dim gb_showTab As New GrammarBuilder()
-            'gb_showTab.Append("Show")
-            'Dim showChoices As New Choices()
-            'For Each tab As TabPage In Form1.TabControlPage1.TabPages
-            '    showChoices.Add(tab.Text.TrimEnd("."c))
-            'Next
-            'For Each tab As TabPage In Form1.TabControlPage2.TabPages
-            '    showChoices.Add(tab.Text.TrimEnd("."c))
-            'Next
-
-            'gb_showTab.Append(showChoices)
-            's_sre.LoadGrammarAsync(New Grammar(gb_showTab))
-
-            Form1.Cursor = Cursors.WaitCursor
-            Application.DoEvents()
-            If String.IsNullOrWhiteSpace(s_speechUserName) Then
-                Dim msg As String = ""
-                msg = $"Speech recognition enabled for {s_firstName}"
-
-                If String.IsNullOrWhiteSpace(oldUserName) Then
-                    msg &= $" for a list of commands say, {ProjectName} what can I say"
-                End If
-
-                Dim p As Prompt = PlayText(msg)
-                While p IsNot Nothing AndAlso Not p.IsCompleted
-                    Threading.Thread.Sleep(10)
-                End While
-                p = Nothing
-            End If
-            s_speechUserName = s_firstName
-            Form1.StatusStripSpeech.Text = "Listening"
-            s_sre.RecognizeAsync(RecognizeMode.Multiple)
-            AddHandler s_sre.SpeechRecognized, AddressOf sre_SpeechRecognized
-
-            Form1.Cursor = Cursors.Default
-            AddHandler s_sre.AudioSignalProblemOccurred, AddressOf sre_AudioSignalProblemOccurred
-        Catch ex As Exception
-            Debug.WriteLine(ex.Message)
-            Stop
-        End Try
-
-    End Sub
-
-    Friend Function PlayText(text As String) As Prompt
-        If Not My.Settings.SystemAudioAlertsEnabled Then
-            Form1.StatusStripSpeech.Text = ""
-            Return Nothing
-        End If
-        If s_lastSpokenMessage = text AndAlso DateDiff(DateInterval.Minute, Now, s_timeOfLastAlert) < s_30SecondInMilliseconds Then
-            Form1.StatusStripSpeech.Text = $"Rejected: '{text}' too soon, Listening"
-            s_StatusStripSpeechText = text
-            Return Nothing
-        End If
-        If Form1.StatusStripSpeech.Text.Contains("too soon") Then
-            Form1.StatusStripSpeech.Text = "Listening"
-        End If
-        s_timeOfLastAlert = Now
-        s_lastSpokenMessage = text
-        If Not s_speechErrorReported Then
-            If s_ss Is Nothing Then
-                InitializeAudioAlerts()
-            End If
-        End If
-        Return s_ss.SpeakAsync(text)
+    Private Function GetTrendText() As String
+        Dim arrows As String = Form1.LabelTrendArrows.Text
+        Dim arrowCount As Integer
+        Select Case True
+            Case arrows.Contains("↓"c)
+                arrowCount = arrows.Count("↓"c)
+                Return $" and is trending down with {arrowCount} Arrow{If(arrowCount > 1, "s", "")}"
+            Case arrows.Contains("↑"c)
+                arrowCount = arrows.Count("↑"c)
+                Return $" and is trending up with {arrowCount} Arrow{If(arrowCount > 1, "s", "")}"
+            Case Else
+                Return " with no trend arrows"
+        End Select
     End Function
 
-    Friend Sub sre_SpeechRecognized(sender As Object, e As SpeechRecognizedEventArgs)
+    Private Sub SpeechRecognized(sender As Object, e As SpeechRecognizedEventArgs)
         If Not My.Settings.SystemSpeechRecognitionEnabled Then
             Form1.StatusStripSpeech.Text = ""
             Exit Sub
@@ -317,5 +202,125 @@ Friend Module SpeechSupport
             Form1.StatusStripSpeech.Text = $"{message}Listening..."
         End If
     End Sub
+
+    Friend Sub CancelSpeechRecognition()
+        If s_sre IsNot Nothing Then
+            RemoveHandler s_sre.AudioSignalProblemOccurred, AddressOf AudioSignalProblemOccurred
+            RemoveHandler s_sre.SpeechRecognized, AddressOf SpeechRecognized
+            s_sre.RecognizeAsyncCancel()
+            s_sre.Dispose()
+            s_sre = Nothing
+            s_speechUserName = ""
+            Form1.StatusStripSpeech.Text = ""
+        End If
+    End Sub
+
+    Friend Sub InitializeAudioAlerts()
+        If s_ss Is Nothing Then
+            s_ss = New SpeechSynthesizer()
+            s_ss.SetOutputToDefaultAudioDevice()
+        End If
+
+    End Sub
+
+    Friend Sub InitializeSpeechRecognition()
+        Dim oldUserName As String = s_speechUserName
+        If s_speechUserName = s_firstName AndAlso s_sre IsNot Nothing Then
+            Exit Sub
+        End If
+        CancelSpeechRecognition()
+
+        Try
+            s_speechWakeWordFound = False
+
+            Dim culture As New CultureInfo("en-us")
+            s_sre = New SpeechRecognitionEngine(culture)
+            s_sre.SetInputToDefaultAudioDevice()
+
+            Dim gb_Attention As New GrammarBuilder With {.Culture = culture}
+            gb_Attention.Append(s_careLinkLower)
+            s_sre.LoadGrammarAsync(New Grammar(gb_Attention))
+
+            'Dim gb_StartStop As New GrammarBuilder With {.Culture = culture}
+            'gb_StartStop.Append("alerts")
+            'gb_StartStop.Append(New Choices("off", "on"))
+            's_sre.LoadGrammarAsync(New Grammar(gb_StartStop))
+
+            Dim gb_what As New GrammarBuilder With {.Culture = culture}
+            gb_what.Append("What")
+            gb_what.Append(New Choices("can I say", "is my SG", "is my BG", "is my Blood Sugar", "is my Blood Glucose"))
+            s_sre.LoadGrammarAsync(New Grammar(gb_what))
+
+            Dim gb_tellMe As New GrammarBuilder With {.Culture = culture}
+            gb_tellMe.Append("Tell me")
+            gb_tellMe.Append($"{s_firstName}'s")
+            gb_tellMe.Append(New Choices("SG", "BG", "Blood Sugar", "Blood Glucose"))
+            s_sre.LoadGrammarAsync(New Grammar(gb_tellMe))
+
+            'Dim gb_showTab As New GrammarBuilder()
+            'gb_showTab.Append("Show")
+            'Dim showChoices As New Choices()
+            'For Each tab As TabPage In Form1.TabControlPage1.TabPages
+            '    showChoices.Add(tab.Text.TrimEnd("."c))
+            'Next
+            'For Each tab As TabPage In Form1.TabControlPage2.TabPages
+            '    showChoices.Add(tab.Text.TrimEnd("."c))
+            'Next
+
+            'gb_showTab.Append(showChoices)
+            's_sre.LoadGrammarAsync(New Grammar(gb_showTab))
+
+            Form1.Cursor = Cursors.WaitCursor
+            Application.DoEvents()
+            If String.IsNullOrWhiteSpace(s_speechUserName) Then
+                Dim msg As String = ""
+                msg = $"Speech recognition enabled for {s_firstName}"
+
+                If String.IsNullOrWhiteSpace(oldUserName) Then
+                    msg &= $" for a list of commands say, {ProjectName} what can I say"
+                End If
+
+                Dim p As Prompt = PlayText(msg)
+                While p IsNot Nothing AndAlso Not p.IsCompleted
+                    Threading.Thread.Sleep(10)
+                End While
+                p = Nothing
+            End If
+            s_speechUserName = s_firstName
+            Form1.StatusStripSpeech.Text = "Listening"
+            s_sre.RecognizeAsync(RecognizeMode.Multiple)
+            AddHandler s_sre.SpeechRecognized, AddressOf SpeechRecognized
+
+            Form1.Cursor = Cursors.Default
+            AddHandler s_sre.AudioSignalProblemOccurred, AddressOf AudioSignalProblemOccurred
+        Catch ex As Exception
+            Debug.WriteLine(ex.Message)
+            Stop
+        End Try
+
+    End Sub
+
+    Friend Function PlayText(text As String) As Prompt
+        If Not My.Settings.SystemAudioAlertsEnabled Then
+            Form1.StatusStripSpeech.Text = ""
+            Return Nothing
+        End If
+        If s_lastSpokenMessage = text AndAlso DateDiff(DateInterval.Minute, Now, s_timeOfLastAlert) < s_30SecondInMilliseconds Then
+            Form1.StatusStripSpeech.Text = $"Rejected: '{text}' too soon, Listening"
+            s_StatusStripSpeechText = text
+            Return Nothing
+        End If
+        If Form1.StatusStripSpeech.Text.Contains("too soon") Then
+            Form1.StatusStripSpeech.Text = "Listening"
+        End If
+        s_timeOfLastAlert = Now
+        s_lastSpokenMessage = text
+        If Not s_speechErrorReported Then
+            If s_ss Is Nothing Then
+                InitializeAudioAlerts()
+            End If
+        End If
+        Return s_ss.SpeakAsync(text)
+    End Function
 
 End Module
