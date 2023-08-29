c93f941289b2ebbefb14736b1c16a288faea8040
Paul M Cohen <paul1956@users.noreply.github.com>
2023-08-28 17:19:49 -0700

    Update AutoSuspendRecord

---
+++ src/CareLink/Records/UserDataRecords/PDF DeviceSettings/AutoSuspendRecord.vb
@@ -0,0 +1,15 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Public Class AutoSuspendRecord
+
+    Public Sub New(lines As List(Of String))
+        Me.Alarm = lines.GetSingleLineValue(Of String)("Auto Suspend ")
+        Me.Alarm = lines.GetSingleLineValue(Of String)("Time ")
+
+    End Sub
+
+    Public Property Alarm As String
+    Public Property Time As TimeSpan
+End Class
