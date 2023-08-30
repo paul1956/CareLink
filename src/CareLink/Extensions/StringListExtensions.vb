2ab43e3560e489976e090ef09b35bf12416f6857
Paul M Cohen <paul1956@users.noreply.github.com>
2023-08-30 14:12:27 -0700

    Update StringListExtensions from branch

--- src/CareLink/Extensions/StringListExtensions.vb
+++ src/CareLink/Extensions/StringListExtensions.vb
@@ -2,6 +2,18 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
-Module StringListExtensions
+Imports System.Runtime.CompilerServices
+
+Public Module StringListExtensions
+
+    <Extension>
+    Public Function FindLineContaining(lines As List(Of String), target As String) As String
+        For Each line As String In lines
+            If line.Contains(target) Then
+                Return line
+            End If
+        Next
+        Return ""
+    End Function
 
 End Module
