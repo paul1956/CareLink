d1511a06b9d80d5041d9bcb74899e5172d16303b
Paul M Cohen <paul1956@users.noreply.github.com>
2023-08-30 14:13:33 -0700

    Update StringTable from Branch

--- src/CareLink/Records/UserDataRecords/PDF DeviceSettings/StringTable.vb
+++ src/CareLink/Records/UserDataRecords/PDF DeviceSettings/StringTable.vb
@@ -2,6 +2,42 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
-Module StringTable
+Imports System.Text
 
-End Module
+Public Class StringTable
+
+    Public ReadOnly Property IsValid As Boolean
+        Get
+            Return Me.Rows.Count > 0
+        End Get
+    End Property
+
+    Public Property Rows As New List(Of Row)
+
+    Public Sub Add(r As Row)
+        Me.Rows.Add(r)
+    End Sub
+
+    <DebuggerDisplay("{GetDebuggerDisplay(),nq}")>
+    Public Class Row
+
+        Public Sub New(columns As List(Of String))
+            _Columns = columns
+        End Sub
+
+        Public Property Columns As List(Of String)
+
+        Private Function GetDebuggerDisplay() As String
+            Dim s As New StringBuilder
+            For Each e As IndexClass(Of String) In _Columns.WithIndex
+                s.Append($"Column {e.Index + 1} = {e.Value}")
+                If Not e.IsLast Then
+                    s.Append(", ")
+                End If
+            Next
+            Return s.ToString()
+        End Function
+
+    End Class
+
+End Class
