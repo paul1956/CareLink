91fa25c8505829c7a09776ce687e74c578fdc1e1
Paul M Cohen <paul1956@users.noreply.github.com>
2025-08-25 13:55:42 -0700

    Fix tests

--- src/CareLink/CustomControls/PercentUpDown.vb
+++ src/CareLink/CustomControls/PercentUpDown.vb
@@ -3,5 +3,19 @@
 ' See the LICENSE file in the project root for more information.
 
 Public Class PercentUpDown
+    Inherits NumericUpDown
+
+    Protected Overrides Sub UpdateEditText()
+        Me.Text = Me.Value.ToString() & " %"
+    End Sub
+
+    Protected Overrides Sub ValidateEditText()
+        Dim s As String = Me.Text.Replace(oldValue:=" %", newValue:="")
+        Dim result As Decimal
+        If Decimal.TryParse(s, result) Then
+            Me.Value = result
+        End If
+        MyBase.ValidateEditText()
+    End Sub
 
 End Class
